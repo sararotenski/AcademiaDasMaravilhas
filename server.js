7e28f3fb5885b1da1daff2b6406c0a1e2044dce0
SARA ROTENSKI PEREIRA <sara_pereira3@estudante.sesisenai.org.br>
2025-10-30 12:31:37 -0300

    criando rotas

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
+//Read
 app.get('/', (req, res) => {
     res.send('<h1>academia</h1>')
+})
+
+app.get('/pagina1', (req, res) => {
+    res.send('<h1>essa é a página 1</h1>')
+})
+
+app.get('/pagina2:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+app.get('/pagina3', (req, res) => {
+    res.sendFile(__dirname, 'index.html')
+})
+
+//Post
+app.post('/pagina1', (req, res) => {
+    const dados = req.body
+    res.send('<h1>Postado com sucesso</h1>')
+})
+
+//Update
+app.put('/pagina1/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+app.patch('/', (req, res) => {
+    res.send(req.params.id)
 })
 
 app.listen(3000)
